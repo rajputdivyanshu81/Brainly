f23f05110ae317b6e70261b984c575b0fcfb3a04
Divyanshu Rajput <divyanshurajput709@gmail.com>
2025-02-08 11:24:44 +0530

    fresh brainly project started

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+
+import express from 'express';
+import jwt from 'jsonwebtoken'
+const app = express();
+
+
+app.post("/api/v1/signup", (req, res) => {
+
+})
+
+app.post("/api/v1/signin", (req, res) => {
+
+})
+
+app.post("/api/v1/content", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.get("/api/v1/content", (req, res) => {
+    res.send("Hello World!")
+})
+
+
+app.delete("/api/v1/content", (req, res) => {
+    res.send("Hello World!")
+})
+
+
+app.post('/api/v1/brain/share', (req, res) => {
+    res.send("Hello World!")
+});
+
+
+app.get('/api/v1/brain/:shareLink', (req, res) => {
+    res.send("Hello World!")
+});
+
+
